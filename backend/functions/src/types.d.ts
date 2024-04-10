0fccb2811dd3887059a558753540bd75afd2f567
Han Xiao <han.xiao@jina.ai>
2024-04-10 14:01:07 -0700

    chore: clean code

--- backend/functions/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'langdetect' {
-    interface DetectionResult {
-        lang: string;
-        prob: number;
-    }
-
-    export function detect(text: string): DetectionResult[];
-    export function detectOne(text: string): string | null;
-}
