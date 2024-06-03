773b948a9ea8333f47772168f499c51c052d869d
yanlong.wang <yanlong.wang@naiver.org>
2024-06-03 18:56:46 +0800

    wip

--- backend/functions/src/cloud-functions/interrogator.ts
+++ backend/functions/src/cloud-functions/interrogator.ts
@@ -48,71 +48,6 @@ export class InterrogatorHost extends RPCHost {
             concurrency: 4,
             maxInstances: 200,
         },
-        openapi: {
-            operation: {
-                parameters: {
-                    'Accept': {
-                        description: `Specifies your preference for the response format. \n\n` +
-                            `Supported formats:\n` +
-                            `- text/event-stream\n` +
-                            `- application/json  or  text/json\n` +
-                            `- text/plain`
-                        ,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-No-Cache': {
-                        description: `Ignores internal cache if this header is specified with a value.`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-Respond-With': {
-                        description: `Specifies the (non-default) form factor of the crawled data you prefer. \n\n` +
-                            `Supported formats:\n` +
-                            `- markdown\n` +
-                            `- html\n` +
-                            `- text\n` +
-                            `- screenshot\n`
-                        ,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-Proxy-Url': {
-                        description: `Specifies your custom proxy if you prefer to use one. \n\n` +
-                            `Supported protocols:\n` +
-                            `- http\n` +
-                            `- https\n` +
-                            `- socks4\n` +
-                            `- socks5\n\n` +
-                            `For authentication, https://user:pass@host:port`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-Set-Cookie': {
-                        description: `Sets cookie(s) to the headless browser for your request. \n\n` +
-                            `Syntax is the same with standard Set-Cookie`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-With-Generated-Alt': {
-                        description: `Enable automatic alt-text generating for images without an meaningful alt-text.\n\n` +
-                            `Note: Does not work when \`X-Respond-With\` is specified`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-With-Images-Summary': {
-                        description: `Enable dedicated summary section for images on the page.`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                    'X-With-links-Summary': {
-                        description: `Enable dedicated summary section for hyper links on the page.`,
-                        in: 'header',
-                        schema: { type: 'string' }
-                    },
-                }
-            }
-        },
         exportInGroup: ['api'],
         tags: ['Interrogator'],
         httpMethod: ['get', 'post'],
@@ -194,8 +129,113 @@ export class InterrogatorHost extends RPCHost {
 
         const content = page.toString();
         const promptChunks = expandImages ? await this.expandMarkdown(content) : [content];
-        promptChunks.unshift('===== Start Of Webpage Content =====\n')
-        promptChunks.push('===== End Of Webpage Content =====\n\n')
+        promptChunks.unshift('===== Start Of Webpage Content =====\n');
+        promptChunks.push('===== End Of Webpage Content =====\n\n');
+        promptChunks.push(`${prompt}`);
+
+        const r = await mdl.exec(promptChunks, inputOptions);
+
+        if (Readable.isReadable(r as any)) {
+            const outputStream = new OutputServerEventStream();
+            (r as any as Readable).pipe(outputStream);
+
+            return outputStream;
+        }
+
+        return assignTransferProtocolMeta(r.trim() + '\n', { contentType: 'text/plain', envelope: null });
+    }
+
+    @RPCMethod({
+        runtime: {
+            cpu: 4,
+            memory: '8GiB',
+            timeoutSeconds: 300,
+            concurrency: 4,
+            maxInstances: 200,
+        },
+        exportInGroup: ['api'],
+        tags: ['Interrogator'],
+        httpMethod: ['get', 'post'],
+        returnType: [String, OutputServerEventStream],
+    })
+    async chatWithReader(
+        @RPCReflect() rpcReflect: RPCReflection,
+        @Ctx() ctx: {
+            req: Request,
+            res: Response,
+        },
+        auth: JinaEmbeddingsAuthDTO,
+        inputOptions: LLMModelOptions,
+        crawlerOptions: CrawlerOptions,
+        @Param('url', {
+            required: true,
+            validate(url: URL) {
+                return url.protocol === 'http:' || url.protocol === 'https:';
+            }
+        }) url: URL,
+        @Param('model', { default: 'gpt-4o' }) model: string,
+        @Param('question', {
+            type: String,
+            required: true,
+            validate(txt: string) {
+                return txt.length > 0 && countGPTToken(txt) <= 2048;
+            }
+        }) prompt: string,
+        @Param('expandImages') expandImages?: boolean,
+    ) {
+        const uid = await auth.solveUID();
+        let chargeAmount = 0;
+
+        if (uid) {
+            const user = await auth.assertUser();
+            if (!(user.wallet.total_balance > 0)) {
+                throw new InsufficientBalanceError(`Account balance not enough to run this query, please recharge.`);
+            }
+
+            const rateLimitPolicy = auth.getRateLimits(rpcReflect.name.toUpperCase()) || [RateLimitDesc.from({
+                occurrence: 40,
+                periodSeconds: 60
+            })];
+
+            const apiRoll = await this.rateLimitControl.simpleRPCUidBasedLimit(
+                rpcReflect, uid, [rpcReflect.name.toUpperCase()],
+                ...rateLimitPolicy
+            );
+
+            rpcReflect.finally(() => {
+                if (chargeAmount) {
+                    auth.reportUsage(chargeAmount, `reader-${rpcReflect.name}`).catch((err) => {
+                        this.logger.warn(`Unable to report usage for ${uid}`, { err: marshalErrorLike(err) });
+                    });
+                    apiRoll.chargeAmount = chargeAmount;
+                }
+            });
+        } else if (ctx.req.ip) {
+            const apiRoll = await this.rateLimitControl.simpleRpcIPBasedLimit(rpcReflect, ctx.req.ip, [rpcReflect.name.toUpperCase()],
+                [
+                    // 5 requests per minute
+                    new Date(Date.now() - 60 * 1000), 5
+                ]
+            );
+
+            rpcReflect.finally(() => {
+                if (chargeAmount) {
+                    apiRoll._ref?.set({
+                        chargeAmount,
+                    }, { merge: true }).catch((err) => this.logger.warn(`Failed to log charge amount in apiRoll`, { err }));
+                }
+            });
+        }
+
+        const mdl = this.llmManager.assertModel(model);
+        const crawlerConf = this.crawler.configure(crawlerOptions);
+        this.threadLocal.set('expandImages', expandImages ?? false);
+        const page = await this.crawler.simpleCrawl(crawlerOptions.respondWith, url, crawlerConf);
+
+        const content = page.toString();
+        const promptChunks = expandImages ? await this.expandMarkdown(content) : [content];
+        promptChunks.unshift('===== Start Of Webpage Content =====\n');
+        promptChunks.push('===== End Of Webpage Content =====\n\n');
         promptChunks.push(`${prompt}`);
 
         const r = await mdl.exec(promptChunks, inputOptions);
--- backend/functions/src/dto/scrapping-options.ts
+++ backend/functions/src/dto/scrapping-options.ts
@@ -1,8 +1,95 @@
-import { AutoCastable, Prop, RPC_CALL_ENVIRONMENT } from 'civkit'; // Adjust the import based on where your decorators are defined
+import { Also, AutoCastable, Prop, RPC_CALL_ENVIRONMENT } from 'civkit'; // Adjust the import based on where your decorators are defined
 import type { Request, Response } from 'express';
 import type { CookieParam } from 'puppeteer';
 import { parseString as parseSetCookieString } from 'set-cookie-parser';
 
+
+@Also({
+    openapi: {
+        operation: {
+            parameters: {
+                'Accept': {
+                    description: `Specifies your preference for the response format.\n\n` +
+                        `Supported formats: \n` +
+                        `- text/event-stream\n` +
+                        `- application/json or text/json\n` +
+                        `- text/plain`
+                    ,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Cache-Tolerance': {
+                    description: `Sets internal cache tolerance in seconds if this header is specified with a integer.`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-No-Cache': {
+                    description: `Ignores internal cache if this header is specified with a value.\n\nEquivalent to X-Cache-Tolerance: 0`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Respond-With': {
+                    description: `Specifies the (non-default) form factor of the crawled data you prefer.\n\n` +
+                        `Supported formats: \n` +
+                        `- markdown\n` +
+                        `- html\n` +
+                        `- text\n` +
+                        `- screenshot\n`
+                    ,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Wait-For-Selector': {
+                    description: `Specifies a CSS selector to wait for the appearance of such an element before returning.\n\n` +
+                        'Example: `X-Wait-For-Selector: .content-block`\n'
+                    ,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Target-Selector': {
+                    description: `Specifies a CSS selector for return target instead of the full html.\n\n` +
+                        'Implies `X-Wait-For-Selector: (same selector)`'
+                    ,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Proxy-Url': {
+                    description: `Specifies your custom proxy if you prefer to use one.\n\n` +
+                        `Supported protocols: \n` +
+                        `- http\n` +
+                        `- https\n` +
+                        `- socks4\n` +
+                        `- socks5\n\n` +
+                        `For authentication, https://user:pass@host:port`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-Set-Cookie': {
+                    description: `Sets cookie(s) to the headless browser for your request. \n\n` +
+                        `Syntax is the same with standard Set-Cookie`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-With-Generated-Alt': {
+                    description: `Enable automatic alt-text generating for images without an meaningful alt-text.\n\n` +
+                        `Note: Does not work when \`X-Respond-With\` is specified`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-With-Images-Summary': {
+                    description: `Enable dedicated summary section for images on the page.`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+                'X-With-links-Summary': {
+                    description: `Enable dedicated summary section for hyper links on the page.`,
+                    in: 'header',
+                    schema: { type: 'string' }
+                },
+            }
+        }
+    }
+})
 export class CrawlerOptions extends AutoCastable {
 
     @Prop({
