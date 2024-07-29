f093db54bf5b7b36fd1e4dccb58ce39ffe316ec5
yanlong.wang <yanlong.wang@naiver.org>
2024-07-29 18:45:10 +0800

    wip

--- backend/functions/src/services/pseudo-transfer.ts
+++ backend/functions/src/services/pseudo-transfer.ts
@@ -1,126 +1,168 @@
-import { AbstractRPCRegistry, perNextTick } from 'civkit';
-import _ from 'lodash';
-import { container, singleton } from 'tsyringe';
+import { AsyncService } from 'civkit';
+import { parentPort, threadId } from 'node:worker_threads';
+type Constructor<T = any> = abstract new (...args: any) => T;
 
-import { isMainThread, Worker, parentPort, workerData, threadId, MessageChannel } from 'node:worker_threads';
+type SpecialMixin = 'Promise' | 'EventEmitter' | 'AsyncIterable';
 
-@singleton()
-export class ThreadedServiceRegistry extends AbstractRPCRegistry {
-
-    override container = container;
-
-    filesToLoad: string[] = [];
-    workers: Worker[] = [];
-
-    ongoingTasks = 0;
-
-    loadInWorker(file: string) {
-        this.filesToLoad.push(file);
-    }
-
-    initMain() {
-
-    }
-
-    createWorker() {
-        const worker = new Worker(__filename, {
-            workerData: {
-                type: this.constructor.name,
-                filesToLoad: this.filesToLoad,
-            }
-        });
-
-        this.workers.push(worker);
-
-        return worker;
-    }
-
-    @perNextTick()
-    notifyOngoingTasks() {
-        if (isMainThread) {
-            return;
-        }
-        parentPort?.postMessage({
-            channel: this.constructor.name,
-            event: 'reportOngoingTasks',
-            data: this.ongoingTasks,
-        });
-    }
-
-    override async exec(name: string, input: object, env?: object): Promise<any> {
-        this.ongoingTasks += 1;
-        this.notifyOngoingTasks();
-
-        try {
-            return await super.exec(name, input, env);
-        } finally {
-            this.ongoingTasks -= 1;
-            this.notifyOngoingTasks();
-        }
-    }
-
-    initWorker() {
-        if (isMainThread) {
-            return;
-        }
-        if (workerData.type !== this.constructor.name) {
-            return;
-        }
-        this.filesToLoad = _.uniq([...(workerData.filesToLoad || []), ...this.filesToLoad]);
-        for (const f of this.filesToLoad) {
-            require(f);
-        }
-
-        this.notifyOngoingTasks();
-        setInterval(() => this.notifyOngoingTasks(), 1000).unref();
-
-        parentPort!.on('message', (msg) => {
-            if (msg?.channel === this.constructor.name && msg?.event === 'exec') {
-                this.ongoingTasks = msg.data;
-            }
-        });
-        parentPort!.once('error', (err) => {
-            console.error(err);
-            process.exit(1);
-        });
-
-        this.emit('ready');
-    }
-
+interface PropertyDescriptor {
+    type: 'property' | 'getter&setter' | 'getter' | 'setter';
+    value?: AnyDescriptor;
+    writable: boolean;
+    enumerable: boolean;
+    configurable: boolean;
 }
 
-const instance = container.resolve(ThreadedServiceRegistry);
-
-export default instance;
-export const { Method: ThreadedMethod, Param, Ctx, RPCReflect } = instance.decorators();
-
-
-const FROM_MESSAGE_PORT_SYMBOL = Symbol('FROM_MESSAGE_PORT');
-
-export function fromMessagePort<T extends abstract new (...args: any) => any>(this: T, port: MessagePort, instant: object): InstanceType<T> {
-    Object.setPrototypeOf(instant, this.prototype);
-
-    return instant as any;
+interface ObjectDescriptor {
+    type: 'object';
+    oid: string;
+    propertyDescriptors: {
+        [key: string]: PropertyDescriptor;
+    };
+    constructorName: string;
+    nativeValue?: any;
+    mixins?: SpecialMixin[];
+}
+interface FunctionDescriptor {
+    type: 'function';
+    oid: string;
+    propertyDescriptors: {
+        [key: string]: PropertyDescriptor;
+    };
+    mixins?: SpecialMixin[];
+}
+interface ValueDescriptor {
+    type: 'primitive';
+    value: number | boolean | string | null | undefined | bigint;
 }
 
-let serial = 0;
-const objTrack = new WeakMap();
-function track(obj: object) {
-    if (typeof obj !== 'object') {
-        return null;
+type AnyDescriptor = ObjectDescriptor | FunctionDescriptor | ValueDescriptor;
+
+const NATIVELY_TRANSFERABLE = [Map, Set, RegExp, BigInt, Date, ArrayBuffer, SharedArrayBuffer, WebAssembly.Module];
+function isNativelyTransferable(input: object) {
+
+    for (const x of NATIVELY_TRANSFERABLE) {
+        if (input instanceof x) {
+            return true;
+        }
     }
 
-    const n = objTrack.get(obj);
+    return false;
+}
 
-    if (n) {
+export class PseudoTransfer extends AsyncService {
+
+    trackedSerialToObject = new Map();
+    trackedObjectToSerial = new WeakMap();
+
+    knownTypes = new Map<string, Constructor>;
+
+    receivedObject = new WeakMap<object, string>;
+
+    serial = 0n;
+
+    primaryPort = parentPort;
+
+    serialToId(n: number | bigint) {
         return `${threadId}__${n}`;
     }
 
-    const newN = ++serial;
-    objTrack.set(obj, newN);
+    idToSerial(id: string) {
+        return BigInt(id.split('__')[1]);
+    }
 
-    return `${threadId}__${newN}`;
+    track(obj: object) {
+        if (typeof obj !== 'object') {
+            throw new Error('Only objects can be tracked.');
+        }
+
+        const n = this.trackedObjectToSerial.get(obj);
+
+        if (n) {
+            return this.serialToId(n);
+        }
+
+        const newId = ++this.serial;
+        this.trackedObjectToSerial.set(obj, newId);
+        this.trackedSerialToObject.set(newId, obj);
+
+        return this.serialToId(newId);
+    }
+
+    drop(id: string) {
+        const n = this.idToSerial(id);
+        this.trackedSerialToObject.delete(n);
+    }
+
+    beware(type: Constructor) {
+        if (this.knownTypes.has(type.name)) {
+            throw new Error(`Duplicated type name: ${type.name}`);
+        }
+        this.knownTypes.set(type.name, type);
+    }
+
+
+    export(input: any): AnyDescriptor {
+        if (typeof input !== 'object' && typeof input !== 'function') {
+            return {
+                type: 'primitive',
+                value: input,
+            };
+        }
+
+        const id = this.track(input);
+        const propertyDescriptors = Object.getOwnPropertyDescriptors(input);
+        const desc = {
+            type: typeof input,
+            constructorName: input.constructor.name,
+            oid: id,
+            propertyDescriptors: {},
+            mixins: [],
+        } as ObjectDescriptor | FunctionDescriptor;
+
+        if (Array.isArray(input)) {
+            (desc as ObjectDescriptor).nativeValue = [];
+        } else if (isNativelyTransferable(input)) {
+            (desc as ObjectDescriptor).nativeValue = input;
+        }
+
+        if (typeof input?.then === 'function') {
+            desc.mixins!.push('Promise');
+        }
+        if (typeof input?.emit === 'function') {
+            desc.mixins!.push('EventEmitter');
+        }
+        if (typeof input?.[Symbol.asyncIterator] === 'function') {
+            desc.mixins!.push('AsyncIterable');
+        }
+
+        for (const [key, descriptor] of Object.entries(propertyDescriptors)) {
+            if (typeof key !== 'string') {
+                continue;
+            }
+            const mappedDesc = {
+                writable: descriptor.writable,
+                enumerable: descriptor.enumerable,
+                configurable: descriptor.configurable,
+            } as typeof desc['propertyDescriptors'][string];
+            if (descriptor.hasOwnProperty('value')) {
+                mappedDesc.type = 'property';
+                mappedDesc.value = this.export(descriptor.value);
+            } else if (descriptor.hasOwnProperty('get') && descriptor.hasOwnProperty('set')) {
+                mappedDesc.type = 'getter&setter';
+            } else if (descriptor.hasOwnProperty('get')) {
+                mappedDesc.type = 'getter';
+            } else if (descriptor.hasOwnProperty('set')) {
+                mappedDesc.type = 'setter';
+            }
+            desc.propertyDescriptors[key] = mappedDesc;
+        }
+
+        return desc;
+    }
+
+
 }
+
 
 export function toMessagePort<T extends abstract new (...args: any) => any>(this: T, instant: InstanceType<T>, upstreamPort = parentPort): MessagePort[] {
     if (!upstreamPort) {
